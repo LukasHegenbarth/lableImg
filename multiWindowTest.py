a90c3ff0f62a4b0a95f38fdb1d6e8d61caa4ae79
Lukas Hegenbarth <lukashegenbarth@hotmail.de>
2020-12-03 12:27:58 +0100

    add param line function and intial params for training

--- multiWindowTest.py
+++ multiWindowTest.py
@@ -26,20 +26,21 @@ class stackedWindow(QWidget):
         self.Stack.addWidget(self.stack2)
         self.Stack.addWidget(self.stack3)
 
-        self.main_vbox = QVBoxLayout(self)
+        self.mainVBox = QVBoxLayout(self)
 
         self.button_layout = QHBoxLayout(self)
         self.button_layout.addWidget(self.button1)
         self.button_layout.addWidget(self.button2)
         self.button_layout.addWidget(self.button3)
+        self.button_layout.setAlignment(Qt.AlignLeft)
 
         self.window_layout = QVBoxLayout(self)
         self.window_layout.addWidget(self.Stack)
 
-        self.main_vbox.addLayout(self.button_layout)
-        self.main_vbox.addLayout(self.window_layout)
+        self.mainVBox.addLayout(self.button_layout)
+        self.mainVBox.addLayout(self.window_layout)
 
-        self.setLayout(self.main_vbox)
+        self.setLayout(self.mainVBox)
         self.button1.clicked.connect(self.button1_fcn)
         self.button2.clicked.connect(self.button2_fcn)
         self.button3.clicked.connect(self.button3_fcn)
@@ -55,16 +56,23 @@ class stackedWindow(QWidget):
     def stack2UI(self):
         hbox = QHBoxLayout(self)
         #TODO add param list for training config 
-        vbox = QVBoxLayout(self)
+        paramVBox = QVBoxLayout(self)
+        #add all necessary param fields 
+        self.addParamLine(paramVBox, 'batch size')
+        self.addParamLine(paramVBox, 'learning rate')
+        self.addParamLine(paramVBox, 'total steps')
+        self.addParamLine(paramVBox, 'warmup steps')
+        self.addParamLine(paramVBox, 'warmup learning rate')
         self.trainingButton = QPushButton('start Training')
         self.trainingStatus = QLabel('Training Status')
+
         self.trainingStatus.setMaximumWidth(250)
-        vbox.addWidget(self.trainingButton)
-        vbox.addWidget(self.trainingStatus)
-        vbox.setAlignment(Qt.AlignTop)
+        paramVBox.addWidget(self.trainingButton)
+        paramVBox.addWidget(self.trainingStatus)
+        paramVBox.setAlignment(Qt.AlignTop)
         self.webEngineView = QWebEngineView()
         self.loadPage()
-        hbox.addLayout(vbox)
+        hbox.addLayout(paramVBox)
         hbox.addWidget(self.webEngineView)
         self.stack2.setLayout(hbox)
 
@@ -73,6 +81,16 @@ class stackedWindow(QWidget):
         label = QLabel('This page shows nothing yet')
         layout.addWidget(label)
         self.stack3.setLayout(layout)
+
+    def addParamLine(self, layout, param):
+        paramBox = QHBoxLayout(self)
+        paramName = QLabel(param)
+        paramName.setMaximumWidth(150)
+        paramBox.addWidget(paramName)
+        lineEdit = QLineEdit()
+        lineEdit.setMaximumWidth(200)
+        paramBox.addWidget(lineEdit)
+        layout.addLayout(paramBox)
 
     def loadPage(self):
         self.webEngineView.load(QUrl('http://localhost:6006/'))
