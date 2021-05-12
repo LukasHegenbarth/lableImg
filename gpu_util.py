c6345b08cb3adc264b41cc4259e9a8978dcff6b7
Lukas Hegenbarth <lukashegenbarth@hotmail.de>
2021-05-12 23:51:15 +0200

    change to subprocess.check_ouput

--- gpu_util.py
+++ gpu_util.py
@@ -1,4 +1,5 @@
 import subprocess
+from typing import Text
 
 class Gpu_Info():
     def __init__(self):
@@ -6,9 +7,10 @@ class Gpu_Info():
         
 
     def get(self):
-        self.sp = subprocess.Popen(['nvidia-smi', '-q', '--display=Utilization,Memory'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out_str = self.sp.communicate()
-        out_list = out_str[0].decode("utf-8").split('\n')
+        # self.sp = subprocess.Popen(['nvidia-smi', '-q', '--display=Utilization,Memory'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.check_output(['nvidia-smi', '-q', '--display=Utilization,Memory'])
+        # out_str = self.sp.communicate()
+        out_list = result.decode("utf-8").split('\n')
         mem_total = out_list[10].split(':')[1]
         mem_used = out_list[11].split(':')[1]
         mem_free = out_list[12].split(':')[1]
@@ -16,7 +18,6 @@ class Gpu_Info():
         gpu_max = out_list[25].split(':')[1]
         gpu_min = out_list[26].split(':')[1]
         gpu_avg = out_list[27].split(':')[1]
-        self.sp.terminate()
 
         return(mem_total, mem_used, mem_free, gpu_max, gpu_min, gpu_avg)
 
