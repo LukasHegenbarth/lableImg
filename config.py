aac4cd31f4f8fd6a583494fd5850c2735042eca7
Lukas Hegenbarth <lukashegenbarth@hotmail.de>
2021-06-14 11:55:26 +0200

    add new detection model path

--- config.py
+++ config.py
@@ -3,12 +3,15 @@ CULLING_ERR_THRES = 0.02
 RANSAC_RESIDUAL_THRES = 0.02
 RANSAC_MAX_TRIALS = 100
 
-VIDEO_WIDTH = 640
+VIDEO_WIDTH = 1080
 #VIDEO_WIDTH = 1080
 
 SKIP_FRAMES = False
 
 #DETECTION_MODEL = '/home/lukas/coding/TensorFlow/workspace/object_detection_640x640_v4/exported-models/my_model/saved_model'
-DETECTION_MODEL = '/home/lukas/training_workspace/training/beet/sessions/20210525_141611/inference_graph/saved_model'
+#DETECTION_MODEL = '/home/lukas/training_workspace/training/beet/sessions/20210525_141611/inference_graph/saved_model'
+#DETECTION_MODEL = '/home/lukas/training_workspace/training/beet/sessions/20210528_134627/inference_graph/saved_model'
+DETECTION_MODEL = '/home/lukas/training_workspace/training/beet/sessions/20210605_001202/inference_graph/saved_model'
+
 DETECTION_CATEGORIES = '/home/lukas/coding/TensorFlow/workspace/object_detection_640x640_v4/annotations/label_map.pbtxt'
 
